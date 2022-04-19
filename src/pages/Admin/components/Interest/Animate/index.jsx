1b0fba271ef6c7906fd4f4c0cb7bfae8c717335e
lu zhu <lu.zhu@hand-china.com>
2022-04-19 14:23:39 +0800

    fix：修改部分样式

---
+++ src/pages/Admin/components/Interest/Animate/index.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-19 14:15:35
+ * @LastEditTime: 2022-04-19 14:15:36
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \react-learning1\src\pages\Admin\components\Interest\Animate\index.jsx
+ */
+import React from 'react'
+
+export default function Animate() {
+  return (
+    <div>Animate</div>
+  )
+}
--- src/pages/Admin/components/Interest/Editor/index.jsx
+++ src/pages/Admin/components/Interest/Editor/index.jsx
@@ -9,11 +9,11 @@ import 'braft-editor/dist/index.css'
 
 const Editor = () => {
   const [editorState, setEditorState] = useState(BraftEditor.createEditorState('<p>你好 <b>世界!</b></p>'))
-  const [outputHTML, setOutputHTML] = useState('<p>你好 <b>世界!</b></p>')
+  // const [outputHTML, setOutputHTML] = useState('<p>你好 <b>世界!</b></p>')
 
   const handleEditorChange = editorState => {
     setEditorState(editorState)
-    setOutputHTML(editorState.toHTML())
+    // setOutputHTML(editorState.toHTML())
   }
 
   const submitContent = (data) => {
@@ -27,8 +27,8 @@ const Editor = () => {
         onChange={handleEditorChange}
         onSave={submitContent}
       />
-      <h5>输出内容</h5>
-      <div className='output-content'>{outputHTML}</div>
+      {/* <h5>输出内容</h5>
+      <div className='output-content'>{outputHTML}</div> */}
     </>
   )
 }
